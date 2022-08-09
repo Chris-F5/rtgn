cd9f4b979020bff131108fc8fccc18fed2833d63
Christopher Lang <christopher.lang.256@gmail.com>
2022-08-09 20:19:37 +0100

    Fix example server

--- examples/top_down_shooter/server.c
+++ examples/top_down_shooter/server.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <fcntl.h>
 
 #include <rtgn/common_types.h>
 #include <rtgn/server.h>
@@ -10,13 +11,12 @@ int main()
 {
     rtgn_Server server;
     rtgn_initServer(&server, SERVER_PORT, 16);
-    
     rtgn_startTickClock(TICK_NANO_DELAY);
+    fcntl(0, F_SETFL, O_NONBLOCK);
     while(fgetc(stdin) == EOF) {
         if(rtgn_tickClock()) {
             rtgn_tickServer(&server);
         }
     }
-
     rtgn_destroyServer(&server);
 }
